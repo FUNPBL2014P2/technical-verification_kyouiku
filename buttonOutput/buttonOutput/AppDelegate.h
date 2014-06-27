6e825d28026261cedb6ac47b77a8c68aae68771f
Shungo Ito <b1012191@fun.ac.jp>
2014-06-27 17:45:58 +0900

    表示ボタンで画像を表示、削除ボタンで画像を非表示 #1

---
+++ buttonOutput/buttonOutput/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  buttonOutput
+//
+//  Created by 伊藤駿吾 on 2014/06/27.
+//  Copyright (c) 2014年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ buttonOutput/buttonOutputTests/buttonOutputTests.m
@@ -0,0 +1,34 @@
+//
+//  buttonOutputTests.m
+//  buttonOutputTests
+//
+//  Created by 伊藤駿吾 on 2014/06/27.
+//  Copyright (c) 2014年 myname. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface buttonOutputTests : XCTestCase
+
+@end
+
+@implementation buttonOutputTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
